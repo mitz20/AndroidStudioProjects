aff639b8a1d0e8994dac7eec4fd65bff06297402
Amit Verma <mitz.lko18@gmail.com>
2016-09-17 20:25:54 +0530

    views part2

--- ViewsPart2/app/src/main/java/com/mitz/viewspart2/MainActivity.java
+++ ViewsPart2/app/src/main/java/com/mitz/viewspart2/MainActivity.java
@@ -6,9 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-    private TextView textView;
-    private Button button;
+public class MainActivity extends AppCompatActivity  {
+//    private TextView textView;
+//    private Button button;
     private int clickCount;
 
     @Override
@@ -16,16 +16,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.activity_main_textView);
-        button = (Button) findViewById(R.id.activity_main_button);
+        final TextView textView = (TextView) findViewById(R.id.activity_main_textView);
+        Button button = (Button) findViewById(R.id.activity_main_button);
 
-        button.setOnClickListener(this);
+//        button.setOnClickListener(this);
+//        button.setOnClickListener(new ButtonClickHandler());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickCount++;
+                textView.setText("Clicked " + clickCount + " time" + ( clickCount != 1 ? "s" : "" ));
+            }
+        });
     }
 
-    public void onClick(View view){
-        if( view.getId() == R.id.activity_main_button ) {
-            clickCount++;
-            textView.setText("Clicked " + clickCount + " time" + ( clickCount != 1 ? "s" : "" ));
-        }
-    }
+//    public void onClick(View view) {
+//            clickCount++;
+//            textView.setText("Clicked " + clickCount + " time" + ( clickCount != 1 ? "s" : "" ));
+//        }
+
+//    private class ButtonClickHandler implements View.OnClickListener {
+//        public void onClick(View view) {
+//            clickCount++;
+//            textView.setText("Clicked " + clickCount + " time" + ( clickCount != 1 ? "s" : "" ));
+//        }
+//    }
 }
