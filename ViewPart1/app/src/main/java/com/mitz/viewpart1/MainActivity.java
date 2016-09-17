216b41d4894910d41bbea814a24441d6b742167e
Amit Verma <mitz.lko18@gmail.com>
2016-09-17 19:18:47 +0530

    all about views

---
+++ ViewPart1/app/src/main/java/com/mitz/viewpart1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.mitz.viewpart1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+        setContentView(R.layout.activity_main);
+    }
+}
